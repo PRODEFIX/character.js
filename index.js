80eb28ff2ed8cbf4a8fa09e52bfdf89cd284ea69
PRODEFIX <prodefix.ytb@gmail.com>
2022-11-27 23:13:44 +0100

    Test

--- index.js
+++ index.js
@@ -134,5 +134,5 @@ module.exports = {
         if (!text || typeof text != "string" || !text.length) throw new Error("Please provide a valid string");
 
         return text.match(/(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g)?.length;
-    },
+    }
 };
